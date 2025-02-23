886fce451ec1094cc71720f6a398c7c7ef258770
Viji <53155323+viji-s@users.noreply.github.com>
2025-02-23 14:21:45 -0800

    Update LearmPomTechniques.java

--- src/LearmPomTechniques.java
+++ src/LearmPomTechniques.java
@@ -4,6 +4,12 @@ public class LearmPomTechniques {
 		public static void main(String[] args){
 			int a=30;
 			int b=60;
+			int c=200;
+			if(a>b) && (a>c){
+				System.Out.Println("A is greatest")
+					}else{
+				System.Out.Println("C is greatest")
+			}
 		}
 
 	}
