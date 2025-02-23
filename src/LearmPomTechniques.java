26b0721d2918f971a0577c3b7fe507e6493f7334
viji-s <viji.senthilkumar34@gmail.com>
2025-02-23 14:26:40 -0800

    Updated the test code

--- src/LearmPomTechniques.java
+++ src/LearmPomTechniques.java
@@ -1,8 +1,18 @@
 
 public class LearmPomTechniques {
 
-	private void psvm() {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args){
+		int a=30;
+		int b=60;
+		int c=200;
+		if(a>b && a>c){
+			System.out.print("A is greatest");
+				}else if(b>a){
+			System.out.println("B is greatest");
+				}
+			else {
+				System.out.println("C is greatest");
+			}
+		}
 	}
-}
+
